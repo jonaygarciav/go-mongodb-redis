0eae8146cce120de54f17153ea7918284b042812
Jonay García <jonaygarciav@gmail.com>
2022-08-14 22:57:02 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,95 @@
+package main
+
+// 👈 Require the packages
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v8"
+	"github.com/jonaygarciav/go-mongodb-redis/config"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+// 👈 Create required variables that we'll re-assign later
+var (
+	server      *gin.Engine
+	ctx         context.Context
+	mongoclient *mongo.Client
+	redisclient *redis.Client
+)
+
+// 👈 Init function that will run before the "main" function
+func init() {
+
+	// 👇 Load the .env variables
+	config, err := config.LoadConfig(".")
+	if err != nil {
+		log.Fatal("Could not load environment variables", err)
+	}
+
+	// 👇 Create a context
+	ctx = context.TODO()
+
+	// 👇 Connect to MongoDB
+	mongoconn := options.Client().ApplyURI(config.DBUri)
+	mongoclient, err := mongo.Connect(ctx, mongoconn)
+
+	if err != nil {
+		panic(err)
+	}
+
+	if err := mongoclient.Ping(ctx, readpref.Primary()); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("MongoDB successfully connected...")
+
+	// 👇 Connect to Redis
+	redisclient = redis.NewClient(&redis.Options{
+		Addr: config.RedisUri,
+	})
+
+	if _, err := redisclient.Ping(ctx).Result(); err != nil {
+		panic(err)
+	}
+
+	err = redisclient.Set(ctx, "test", "Welcome to Golang with Redis and MongoDB", 0).Err()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Redis client connected successfully...")
+
+	// 👇 Create the Gin Engine instance
+	server = gin.Default()
+}
+
+func main() {
+	config, err := config.LoadConfig(".")
+
+	if err != nil {
+		log.Fatal("Could not load config", err)
+	}
+
+	defer mongoclient.Disconnect(ctx)
+
+	value, err := redisclient.Get(ctx, "test").Result()
+
+	if err == redis.Nil {
+		fmt.Println("key: test does not exist")
+	} else if err != nil {
+		panic(err)
+	}
+
+	router := server.Group("/api")
+	router.GET("/healthchecker", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{"status": "success", "message": value})
+	})
+
+	log.Fatal(server.Run(":" + config.Port))
+}
